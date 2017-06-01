842d2f12236aa1206b749033139669211e98d87c
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-06-01 17:20:51 +0100

    New examples added

---
+++ src/r-test.R
@@ -0,0 +1,31 @@
+#examples
+# Print Hello World.
+#print("Hello World")
+#print(23.9 + 11.6)
+#install.packages("plotrix")
+
+myString <- "Hello, World!"
+print( myString) 
+
+#if(FALSE) {
+#  "This is a demo for multi-line comments and it should be put inside either a single
+#  OR double quote"
+#}
+v <- TRUE 
+print(class(v))
+
+# Create a vector.
+apple <- c('red','green',"yellow")
+print(apple)
+
+# Get the class of the vector.
+print(class(apple))
+
+# Create a list.
+list1 <- list(c(2,5,3),21.3,sin)
+
+# Print the list.
+print(list1)
+
+
+
